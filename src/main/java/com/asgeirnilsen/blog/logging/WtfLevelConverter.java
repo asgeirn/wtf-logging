66f977bf15beb1cea4b46927bd6b74edbde2313a
Asgeir Storesund Nilsen <asgeir@twingine.no>
2011-12-08 22:16:08 +0100

    Initial commit.

---
+++ src/main/java/com/asgeirnilsen/blog/logging/WtfLevelConverter.java
@@ -0,0 +1,25 @@
+package com.asgeirnilsen.blog.logging;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+public class WtfLevelConverter extends ClassicConverter {
+
+    @Override
+    public String convert(ILoggingEvent event) {
+        switch (event.getLevel().levelInt) {
+        case Level.ERROR_INT:
+            return "OMG";
+        case Level.WARN_INT:
+            return "WTF";
+        case Level.INFO_INT:
+            return "FYI";
+        case Level.DEBUG_INT:
+            return "BTW";
+        default:
+            return event.getLevel().levelStr;
+        }
+    }
+
+}
